b4e21f9b411d8c27ca4d720b23fdabb94d89dded
akash <akashbora122@gmail.com>
2019-05-25 04:04:05 -0700

    test commit

--- app/src/main/java/com/dev/r19/eeragijn/HomeToSelect.java
+++ app/src/main/java/com/dev/r19/eeragijn/HomeToSelect.java
@@ -1,13 +1,19 @@
 package com.dev.r19.eeragijn;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeToSelect extends AppCompatActivity {
 
+    TextView home;
+    Animation blink;
     Button admin, emp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class HomeToSelect extends AppCompatActivity {
         setContentView(R.layout.activity_home_to_select);
         admin = (Button)findViewById(R.id.AsAdmin);
         emp = (Button)findViewById(R.id.AsEmp);
+
+        Typeface mmedium = Typeface.createFromAsset(getAssets(), "fonts/MontserratMedium.ttf");//font style
+
+        blink = AnimationUtils.loadAnimation(this , R.anim.blink_anim);
+        home = (TextView) findViewById (R.id.home);
+        home.startAnimation(blink);
+
+        home.setTypeface(mmedium);//text style
+
         // on click listener for admin
         admin.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/dev/r19/eeragijn/UserHome.java
+++ app/src/main/java/com/dev/r19/eeragijn/UserHome.java
@@ -1,10 +1,14 @@
 package com.dev.r19.eeragijn;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,7 +16,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class UserHome extends AppCompatActivity {
 
     private Button login, reg;
-
+    private TextView userHome;
+    private Animation rotate;
     private FirebaseUser auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,13 @@ public class UserHome extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
         //initialization
 
+        Typeface mmedium = Typeface.createFromAsset(getAssets(), "fonts/MontserratMedium.ttf");//font style
+
+        userHome = (TextView) findViewById(R.id.userHome);
         login = (Button)findViewById(R.id.toUserLogin);
         reg = (Button)findViewById(R.id.toUserReg);
+
+        userHome.setTypeface(mmedium);//text style
 
         // checking for session status
         auth = FirebaseAuth.getInstance().getCurrentUser();
@@ -30,10 +40,17 @@ public class UserHome extends AppCompatActivity {
             startActivity(intent);
         }
 
+        //text animation
+        rotate = AnimationUtils.loadAnimation(this , R.anim.rotate_1);
+        userHome = (TextView) findViewById(R.id.userHome);
+        userHome.startAnimation(rotate);
+
         // start link page user login
       login.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
+              Animation animation = AnimationUtils.loadAnimation(UserHome.this , R.anim.bounce);
+              login.startAnimation(animation);
               Intent intent = new Intent(UserHome.this, UserLogin.class);
               startActivity(intent);
           }
@@ -43,6 +60,8 @@ public class UserHome extends AppCompatActivity {
        reg.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+               Animation animation = AnimationUtils.loadAnimation(UserHome.this , R.anim.bounce);
+               reg.startAnimation(animation);
                Intent intent = new Intent(UserHome.this, UserRegistration.class);
                startActivity(intent);
            }
